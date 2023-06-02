d4e3f185888249854c898eeb8b5f1e63c27d3d6c
Joseph <aelphaeis@live.com>
2023-06-02 02:29:59 -0400

    Pull it all together.

--- src/main/java/com/cruat/lost/ark/market/App.java
+++ src/main/java/com/cruat/lost/ark/market/App.java
@@ -1,12 +1,41 @@
 package com.cruat.lost.ark.market;
 
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.util.regex.Pattern;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
+import com.sun.jna.platform.win32.WinDef.HWND;
+
 /**
  * Hello world!
  *
  */
 public class App {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void main(String... args) {
+		// TODO improve this REGEX
+		Pattern regex = Pattern.compile("LOST ARK");
+
+		// Lets resolve our Handle
+		HWND handle = new ProcessNameResolver(regex)
+				.getFirstHandle()
+				.orElseThrow(PrototypeException::new);
+
+		Camera camera = new Camera(handle);
+		show(camera.capture());
+	}
+
+	public static void show(BufferedImage image) {
+		JFrame frame = new JFrame();
+		frame.getContentPane().setLayout(new FlowLayout());
+		frame.getContentPane().add(new JLabel(new ImageIcon(image)));
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 	}
 }
