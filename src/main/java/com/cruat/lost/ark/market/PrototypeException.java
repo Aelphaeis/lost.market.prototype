fe6a4e07555a9bbd7550441a0fbc3f042bee0016
Joseph <aelphaeis@live.com>
2023-06-02 02:00:44 -0400

    We'll need an exception

---
+++ src/main/java/com/cruat/lost/ark/market/PrototypeException.java
@@ -0,0 +1,22 @@
+package com.cruat.lost.ark.market;
+
+public class PrototypeException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public PrototypeException() {
+		super();
+	}
+
+	public PrototypeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public PrototypeException(String message) {
+		super(message);
+	}
+
+	public PrototypeException(Throwable cause) {
+		super(cause);
+	}
+}
